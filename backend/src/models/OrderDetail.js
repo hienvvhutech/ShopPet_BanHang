dee71f490210a1ab3f41d1123879d85c96266c01
HGBAO-DEV <huynhgiabao.2305@gmail.com>
2026-04-05 02:30:54 +0700

    update order và test api thành công

--- backend/src/models/OrderDetail.js
+++ backend/src/models/OrderDetail.js
@@ -1,10 +1,24 @@
 const mongoose = require('mongoose');
 
 const orderDetailSchema = new mongoose.Schema({
-    order_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Order', required: true },
-    product_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-    quantity: { type: Number, required: true, default: 1 },
-    price: { type: Number, required: true }
-}, { timestamps: true });
+    order_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Order',
+        required: true
+    },
+    product_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+});
 
 module.exports = mongoose.model('OrderDetail', orderDetailSchema);
